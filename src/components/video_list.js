6b850260aee942fa83a12bf3587e6228882bc39a
Silviu <v@f.c>
2016-09-09 19:05:40 +0300

    video list item

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 //class VideoList extends Component {
 //    render() {
@@ -12,9 +13,14 @@ import React from 'react';
 
 //will not have state, so functional
 const VideoList = (props) => {
+
+    const videoItems = props.videos.map(video => {
+        return <VideoListItem key={video.id.videoId} video={video}/>
+    });
+
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+            {videoItems}
         </ul>
     );
 };
